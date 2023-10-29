2b2653864fc802f048506d50d2a52958a945ed5d
Documenter.jl <documenter@juliadocs.github.io>
2023-10-29 12:17:55 +0000

    build based on ae28e4d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SIMReconstruction","category":"page"},{"location":"#SIMReconstruction","page":"Home","title":"SIMReconstruction","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SIMReconstruction.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SIMReconstruction]","category":"page"}]
+}
